28bad5bad4a33f8f5beb40a564bfb3681d58bbaa
Sankalp Pandey <82685501+sanpdy@users.noreply.github.com>
2025-04-04 20:09:35 -0500

    added welcome screen, base zen and grind screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,23 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Text } from 'react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs screenOptions={{
+      headerShown: false,
+    }}>
       <Tabs.Screen
-        name="index"
+        name="zen"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'ZEN',
+          tabBarIcon: () => <Text>🧘‍♂️</Text>,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="grind"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'GRIND',
+          tabBarIcon: () => <Text>🏋️</Text>,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/zen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function zen_screen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>🧘‍♂️ ZEN</Text>
+    </View>
+  );
+}
